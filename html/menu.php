354d08cf0ade1d9b30ea03da74424a1943dcc820
LysergMoonlighting <68394160+LysergMoonlighting@users.noreply.github.com>
2021-02-18 12:23:56 +0100

    Progreso de el entrono centrald e la página
    se hja añadido el menu, scroll style y estoy en progreso de editar el varios scrips donde cambia al vision cuando se hace scroll

---
+++ html/menu.php
@@ -0,0 +1,99 @@
+
+<nav class="navbar smart-scroll navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="../index.php"><img src="../imagenes/logo.png"   width="60" ></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="../index.php">HAZ EL AMOR Y NO LA GUERRA <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Examen 1º trimestre
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="../Examen/Seccion3.php">Acerca de</a>
+          <a class="dropdown-item" href="../Examen/Seccion4.php">Servicios</a>
+          <a class="dropdown-item" href="../Examen/Seccion5.php">Tarifas</a>
+          <a class="dropdown-item" href="../Examen/Seccion6.php">Multimedia</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="../Examen/Seccion7.php">Contacta con nosotros</a>
+        </div>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          UA3
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="../ua3/ejercicio1.php">Tarea 1.1 - selectores</a>
+          <a class="dropdown-item" href="../ua3/ejercicio2.php">Tarea 1.2 - selectores</a>
+          <a class="dropdown-item" href="../ua3/ejercicio3.php">Tarea 1.3 - selectores</a>
+          <a class="dropdown-item" href="../ua3/ejerciciomargen1.php">Tarea 2.1 - Ejercicios de margin, padding y bordes</a>
+      </li>
+      
+      <li class="nav-item">    
+      <a class="nav-link" href="#">Esto no hace nada</a>
+      </li>
+    </ul>
+   
+  </div>
+</nav>
+
+
+
+
+
+<div class="wrapper position-fixed bg-dark">
+    <!-- Sidebar -->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>Bootstrap Sidebar</h3>
+        </div>
+
+        <ul class="list-unstyled components">
+            <p>Dummy Heading</p>
+            <li class="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                <ul class="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="#">Home 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+                <ul class="collapse list-unstyled" id="pageSubmenu">
+                    <li>
+                        <a href="#">Page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+
+</div>
---
+++ index.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html lang="es" xml:lang="es">
+	<head>
+    <?php include("html/meta.php")?>
+    <?php include("html/menu.php")?>
+	</head>
+  <body>
+    
+  
+    <div class="seccion">
+    <h2 class="titulo">HAZ EL AMOR Y NO LA GUERRA</h2>
+    <div class="contenido">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porttitor libero vitae turpis accumsan, vitae tincidunt arcu ultrices. Vestibulum nec mi scelerisque, rhoncus tortor et, hendrerit felis. Praesent odio nulla, efficitur vel mi id, pretium porttitor lectus. Aliquam erat volutpat. Mauris tellus nisl, placerat nec convallis eu, auctor eget est. Cras interdum lorem augue, ut cursus ex semper nec. Nam in urna facilisis, pharetra elit vel, malesuada lectus. Aenean faucibus interdum tortor, vitae malesuada est semper nec. Sed vehicula sapien pellentesque ligula tincidunt varius. Vivamus id dapibus enim. Suspendisse venenatis, turpis ac porttitor viverra, sapien nunc blandit mauris, a blandit urna leo ac sem. Sed et dapibus lectus, non dignissim libero. Nulla in nibh quis dolor tempor elementum pretium sit amet erat. Fusce eu ante eu lorem gravida vulputate sed quis libero.
+
+Aliquam eu suscipit tellus. Integer vel tristique nisi. Sed eu condimentum est. Praesent malesuada nibh at metus facilisis accumsan. Nullam ac convallis massa. Curabitur suscipit, tellus aliquet sollicitudin ultrices, risus eros finibus sem, vitae malesuada mauris risus vitae libero. Phasellus fringilla diam a arcu auctor scelerisque. Morbi dignissim elit tincidunt, pretium quam id, euismod nulla.
+
+Ut aliquam, leo nec placerat semper, enim purus auctor massa, quis rutrum eros diam sit amet nulla. Donec at fermentum massa. Integer rhoncus ex nec odio pulvinar, eget consequat quam volutpat. Mauris quam diam, bibendum ut sagittis sed, interdum a eros. Cras mauris nunc, sagittis quis tincidunt a, bibendum vitae dolor. Quisque volutpat purus pulvinar turpis efficitur, in sollicitudin arcu pulvinar. Fusce non tempus nunc.
+
+Morbi eget turpis imperdiet, gravida arcu in, vehicula arcu. Quisque a maximus dolor, ut faucibus nibh. In consequat cursus fringilla. Donec blandit nisi leo, ut placerat lectus facilisis vitae. Morbi sed ligula ut nunc blandit semper. Proin et justo ultrices, vestibulum ex vel, placerat ex. Curabitur viverra vitae arcu faucibus elementum.
+
+Cras ut neque dui. Mauris aliquet sapien sit amet eros placerat tempor. Ut condimentum turpis justo, vel molestie leo commodo eu. Maecenas vitae rutrum nunc, id cursus massa. Praesent aliquet risus risus, quis fringilla elit lobortis in. Sed a ex eget est lobortis rutrum. Aenean vel convallis libero. Quisque augue velit, tristique vitae libero sed, porta dignissim diam. Aenean eleifend commodo mauris, sed suscipit tellus porta sed. Integer lacinia ipsum sed dignissim facilisis. Curabitur eu metus ultrices, faucibus quam a, suscipit nisi. Suspendisse a placerat nulla, a vestibulum enim. Aliquam nec dolor quis nunc porttitor vulputate. In hac habitasse platea dictumst.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porttitor libero vitae turpis accumsan, vitae tincidunt arcu ultrices. Vestibulum nec mi scelerisque, rhoncus tortor et, hendrerit felis. Praesent odio nulla, efficitur vel mi id, pretium porttitor lectus. Aliquam erat volutpat. Mauris tellus nisl, placerat nec convallis eu, auctor eget est. Cras interdum lorem augue, ut cursus ex semper nec. Nam in urna facilisis, pharetra elit vel, malesuada lectus. Aenean faucibus interdum tortor, vitae malesuada est semper nec. Sed vehicula sapien pellentesque ligula tincidunt varius. Vivamus id dapibus enim. Suspendisse venenatis, turpis ac porttitor viverra, sapien nunc blandit mauris, a blandit urna leo ac sem. Sed et dapibus lectus, non dignissim libero. Nulla in nibh quis dolor tempor elementum pretium sit amet erat. Fusce eu ante eu lorem gravida vulputate sed quis libero.
+
+Aliquam eu suscipit tellus. Integer vel tristique nisi. Sed eu condimentum est. Praesent malesuada nibh at metus facilisis accumsan. Nullam ac convallis massa. Curabitur suscipit, tellus aliquet sollicitudin ultrices, risus eros finibus sem, vitae malesuada mauris risus vitae libero. Phasellus fringilla diam a arcu auctor scelerisque. Morbi dignissim elit tincidunt, pretium quam id, euismod nulla.
+
+Ut aliquam, leo nec placerat semper, enim purus auctor massa, quis rutrum eros diam sit amet nulla. Donec at fermentum massa. Integer rhoncus ex nec odio pulvinar, eget consequat quam volutpat. Mauris quam diam, bibendum ut sagittis sed, interdum a eros. Cras mauris nunc, sagittis quis tincidunt a, bibendum vitae dolor. Quisque volutpat purus pulvinar turpis efficitur, in sollicitudin arcu pulvinar. Fusce non tempus nunc.
+
+Morbi eget turpis imperdiet, gravida arcu in, vehicula arcu. Quisque a maximus dolor, ut faucibus nibh. In consequat cursus fringilla. Donec blandit nisi leo, ut placerat lectus facilisis vitae. Morbi sed ligula ut nunc blandit semper. Proin et justo ultrices, vestibulum ex vel, placerat ex. Curabitur viverra vitae arcu faucibus elementum.
+
+Cras ut neque dui. Mauris aliquet sapien sit amet eros placerat tempor. Ut condimentum turpis justo, vel molestie leo commodo eu. Maecenas vitae rutrum nunc, id cursus massa. Praesent aliquet risus risus, quis fringilla elit lobortis in. Sed a ex eget est lobortis rutrum. Aenean vel convallis libero. Quisque augue velit, tristique vitae libero sed, porta dignissim diam. Aenean eleifend commodo mauris, sed suscipit tellus porta sed. Integer lacinia ipsum sed dignissim facilisis. Curabitur eu metus ultrices, faucibus quam a, suscipit nisi. Suspendisse a placerat nulla, a vestibulum enim. Aliquam nec dolor quis nunc porttitor vulputate. In hac habitasse platea dictumst.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porttitor libero vitae turpis accumsan, vitae tincidunt arcu ultrices. Vestibulum nec mi scelerisque, rhoncus tortor et, hendrerit felis. Praesent odio nulla, efficitur vel mi id, pretium porttitor lectus. Aliquam erat volutpat. Mauris tellus nisl, placerat nec convallis eu, auctor eget est. Cras interdum lorem augue, ut cursus ex semper nec. Nam in urna facilisis, pharetra elit vel, malesuada lectus. Aenean faucibus interdum tortor, vitae malesuada est semper nec. Sed vehicula sapien pellentesque ligula tincidunt varius. Vivamus id dapibus enim. Suspendisse venenatis, turpis ac porttitor viverra, sapien nunc blandit mauris, a blandit urna leo ac sem. Sed et dapibus lectus, non dignissim libero. Nulla in nibh quis dolor tempor elementum pretium sit amet erat. Fusce eu ante eu lorem gravida vulputate sed quis libero.
+
+Aliquam eu suscipit tellus. Integer vel tristique nisi. Sed eu condimentum est. Praesent malesuada nibh at metus facilisis accumsan. Nullam ac convallis massa. Curabitur suscipit, tellus aliquet sollicitudin ultrices, risus eros finibus sem, vitae malesuada mauris risus vitae libero. Phasellus fringilla diam a arcu auctor scelerisque. Morbi dignissim elit tincidunt, pretium quam id, euismod nulla.
+
+Ut aliquam, leo nec placerat semper, enim purus auctor massa, quis rutrum eros diam sit amet nulla. Donec at fermentum massa. Integer rhoncus ex nec odio pulvinar, eget consequat quam volutpat. Mauris quam diam, bibendum ut sagittis sed, interdum a eros. Cras mauris nunc, sagittis quis tincidunt a, bibendum vitae dolor. Quisque volutpat purus pulvinar turpis efficitur, in sollicitudin arcu pulvinar. Fusce non tempus nunc.
+
+Morbi eget turpis imperdiet, gravida arcu in, vehicula arcu. Quisque a maximus dolor, ut faucibus nibh. In consequat cursus fringilla. Donec blandit nisi leo, ut placerat lectus facilisis vitae. Morbi sed ligula ut nunc blandit semper. Proin et justo ultrices, vestibulum ex vel, placerat ex. Curabitur viverra vitae arcu faucibus elementum.
+
+Cras ut neque dui. Mauris aliquet sapien sit amet eros placerat tempor. Ut condimentum turpis justo, vel molestie leo commodo eu. Maecenas vitae rutrum nunc, id cursus massa. Praesent aliquet risus risus, quis fringilla elit lobortis in. Sed a ex eget est lobortis rutrum. Aenean vel convallis libero. Quisque augue velit, tristique vitae libero sed, porta dignissim diam. Aenean eleifend commodo mauris, sed suscipit tellus porta sed. Integer lacinia ipsum sed dignissim facilisis. Curabitur eu metus ultrices, faucibus quam a, suscipit nisi. Suspendisse a placerat nulla, a vestibulum enim. Aliquam nec dolor quis nunc porttitor vulputate. In hac habitasse platea dictumst.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porttitor libero vitae turpis accumsan, vitae tincidunt arcu ultrices. Vestibulum nec mi scelerisque, rhoncus tortor et, hendrerit felis. Praesent odio nulla, efficitur vel mi id, pretium porttitor lectus. Aliquam erat volutpat. Mauris tellus nisl, placerat nec convallis eu, auctor eget est. Cras interdum lorem augue, ut cursus ex semper nec. Nam in urna facilisis, pharetra elit vel, malesuada lectus. Aenean faucibus interdum tortor, vitae malesuada est semper nec. Sed vehicula sapien pellentesque ligula tincidunt varius. Vivamus id dapibus enim. Suspendisse venenatis, turpis ac porttitor viverra, sapien nunc blandit mauris, a blandit urna leo ac sem. Sed et dapibus lectus, non dignissim libero. Nulla in nibh quis dolor tempor elementum pretium sit amet erat. Fusce eu ante eu lorem gravida vulputate sed quis libero.
+
+Aliquam eu suscipit tellus. Integer vel tristique nisi. Sed eu condimentum est. Praesent malesuada nibh at metus facilisis accumsan. Nullam ac convallis massa. Curabitur suscipit, tellus aliquet sollicitudin ultrices, risus eros finibus sem, vitae malesuada mauris risus vitae libero. Phasellus fringilla diam a arcu auctor scelerisque. Morbi dignissim elit tincidunt, pretium quam id, euismod nulla.
+
+Ut aliquam, leo nec placerat semper, enim purus auctor massa, quis rutrum eros diam sit amet nulla. Donec at fermentum massa. Integer rhoncus ex nec odio pulvinar, eget consequat quam volutpat. Mauris quam diam, bibendum ut sagittis sed, interdum a eros. Cras mauris nunc, sagittis quis tincidunt a, bibendum vitae dolor. Quisque volutpat purus pulvinar turpis efficitur, in sollicitudin arcu pulvinar. Fusce non tempus nunc.
+
+Morbi eget turpis imperdiet, gravida arcu in, vehicula arcu. Quisque a maximus dolor, ut faucibus nibh. In consequat cursus fringilla. Donec blandit nisi leo, ut placerat lectus facilisis vitae. Morbi sed ligula ut nunc blandit semper. Proin et justo ultrices, vestibulum ex vel, placerat ex. Curabitur viverra vitae arcu faucibus elementum.
+
+Cras ut neque dui. Mauris aliquet sapien sit amet eros placerat tempor. Ut condimentum turpis justo, vel molestie leo commodo eu. Maecenas vitae rutrum nunc, id cursus massa. Praesent aliquet risus risus, quis fringilla elit lobortis in. Sed a ex eget est lobortis rutrum. Aenean vel convallis libero. Quisque augue velit, tristique vitae libero sed, porta dignissim diam. Aenean eleifend commodo mauris, sed suscipit tellus porta sed. Integer lacinia ipsum sed dignissim facilisis. Curabitur eu metus ultrices, faucibus quam a, suscipit nisi. Suspendisse a placerat nulla, a vestibulum enim. Aliquam nec dolor quis nunc porttitor vulputate. In hac habitasse platea dictumst.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porttitor libero vitae turpis accumsan, vitae tincidunt arcu ultrices. Vestibulum nec mi scelerisque, rhoncus tortor et, hendrerit felis. Praesent odio nulla, efficitur vel mi id, pretium porttitor lectus. Aliquam erat volutpat. Mauris tellus nisl, placerat nec convallis eu, auctor eget est. Cras interdum lorem augue, ut cursus ex semper nec. Nam in urna facilisis, pharetra elit vel, malesuada lectus. Aenean faucibus interdum tortor, vitae malesuada est semper nec. Sed vehicula sapien pellentesque ligula tincidunt varius. Vivamus id dapibus enim. Suspendisse venenatis, turpis ac porttitor viverra, sapien nunc blandit mauris, a blandit urna leo ac sem. Sed et dapibus lectus, non dignissim libero. Nulla in nibh quis dolor tempor elementum pretium sit amet erat. Fusce eu ante eu lorem gravida vulputate sed quis libero.
+
+Aliquam eu suscipit tellus. Integer vel tristique nisi. Sed eu condimentum est. Praesent malesuada nibh at metus facilisis accumsan. Nullam ac convallis massa. Curabitur suscipit, tellus aliquet sollicitudin ultrices, risus eros finibus sem, vitae malesuada mauris risus vitae libero. Phasellus fringilla diam a arcu auctor scelerisque. Morbi dignissim elit tincidunt, pretium quam id, euismod nulla.
+
+Ut aliquam, leo nec placerat semper, enim purus auctor massa, quis rutrum eros diam sit amet nulla. Donec at fermentum massa. Integer rhoncus ex nec odio pulvinar, eget consequat quam volutpat. Mauris quam diam, bibendum ut sagittis sed, interdum a eros. Cras mauris nunc, sagittis quis tincidunt a, bibendum vitae dolor. Quisque volutpat purus pulvinar turpis efficitur, in sollicitudin arcu pulvinar. Fusce non tempus nunc.
+
+Morbi eget turpis imperdiet, gravida arcu in, vehicula arcu. Quisque a maximus dolor, ut faucibus nibh. In consequat cursus fringilla. Donec blandit nisi leo, ut placerat lectus facilisis vitae. Morbi sed ligula ut nunc blandit semper. Proin et justo ultrices, vestibulum ex vel, placerat ex. Curabitur viverra vitae arcu faucibus elementum.
+
+Cras ut neque dui. Mauris aliquet sapien sit amet eros placerat tempor. Ut condimentum turpis justo, vel molestie leo commodo eu. Maecenas vitae rutrum nunc, id cursus massa. Praesent aliquet risus risus, quis fringilla elit lobortis in. Sed a ex eget est lobortis rutrum. Aenean vel convallis libero. Quisque augue velit, tristique vitae libero sed, porta dignissim diam. Aenean eleifend commodo mauris, sed suscipit tellus porta sed. Integer lacinia ipsum sed dignissim facilisis. Curabitur eu metus ultrices, faucibus quam a, suscipit nisi. Suspendisse a placerat nulla, a vestibulum enim. Aliquam nec dolor quis nunc porttitor vulputate. In hac habitasse platea dictumst.
+   
+</div>  
+</div> 
+<script>
+// add padding top to show content behind navbar
+$('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
+</script>
+
+</html> 
+
+
+
+
