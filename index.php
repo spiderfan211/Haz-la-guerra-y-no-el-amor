ce05ebacb502a804bef6430f0986bc470d6c6652
Emilio <spiderfan211@gmail.com>
2021-02-21 03:46:59 +0100

    Maquetación y añadido de imágenes al index.php
    He añadido las imágenes y enlaces correspondientes a los hexágonos de index.php. Ahora tienen una imagen representativa de adonde señalan, personajes, historia, las dos facciones y nuestra información de contacto. Aún se tiene que lograr centrar el conjunto de los hexágonos en la galería. También es el primer verdadero commit en la nueva rama.

--- index.php
+++ index.php
@@ -14,23 +14,23 @@
 
     <div class = "gallery">
             <div class="clipped-border">
-                <img src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.gettyimages.com.au%2Fgi-resources%2Fimages%2Ffrontdoor%2Fcreative%2FPanoramicImagesRM%2FFD_image.jpg&f=1" id="clipped">
+                <a href="./contenido/personajes.php"><img src="./imagenes/malo maloso.gif" id="clipped"></a>
             </div>
             
             <div class="clipped-border">
-                <img src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fbpucette.b.p.pic.centerblog.net%2Ffv86ll9r.jpg&f=1" id="clipped">
+                <a href="./contenido/orden.php"><img src="./imagenes/orden.png" id="clipped"></a>
             </div>
 
             <div class="clipped-border">
-                <img src="imagenes/sans.png" id="clipped">
+                <a href="./contenido/historia.php"><img src="./imagenes/historia.jpg" id="clipped"></a>
             </div>
 
             <div class="clipped-border">
-                <img src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.lokeshdhakar.com%2Fprojects%2Flightbox2%2Fimages%2Fimage-4.jpg&f=1" id="clipped">
+                <a href="./contenido/nosotros.php"><img src="./imagenes/nosotros.png" id="clipped"></a>
             </div>
 
             <div class="clipped-border">
-                <img src="https://images.pexels.com/photos/932638/pexels-photo-932638.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" id="clipped">
+                <a href="./contenido/alianza.php"><img src="./imagenes/alianza.png" id="clipped"></a>
             </div>
 
         <div class = "shadow"></div>
