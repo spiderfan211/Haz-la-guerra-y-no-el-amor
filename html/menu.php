857ccacb65ad07372f26444957043655c906181c
LysergMoonlighting <lysergmoonlighting@gmail.com>
2021-02-21 18:48:44 +0100

    comprobacion en distintas pantalals

--- html/menu.php
+++ html/menu.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top" alt="Max-width 10%">
   <a class="navbar-brand" href="../../../index.php"><img src="../../../imagenes/logo.png"   width="60" ></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
