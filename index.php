0565e97b57eb285f11aff18bc572929293121035
LysergMoonlighting <lysergmoonlighting@gmail.com>
2021-02-21 19:24:11 +0100

    otra prubea de resoluciones

--- index.php
+++ index.php
@@ -12,29 +12,29 @@
     
 
 
-    <div class = "gallery">
-            <div class="clipped-border">
-                <a href="./contenido/personajes.php"><img src="./imagenes/malo maloso.gif" id="clipped"></a>
-            </div>
-            
-            <div class="clipped-border">
-                <a href="./contenido/orden.php"><img src="./imagenes/orden.png" id="clipped"></a>
-            </div>
+            <div class = "gallery">
+                    <div class="clipped-border">
+                        <a href="./contenido/personajes.php"><img src="./imagenes/malo maloso.gif" id="clipped"></a>
+                    </div>
+                    
+                    <div class="clipped-border">
+                        <a href="./contenido/orden.php"><img src="./imagenes/orden.png" id="clipped"></a>
+                    </div>
 
-            <div class="clipped-border">
-                <a href="./contenido/historia.php"><img src="./imagenes/historia.jpg" id="clipped"></a>
-            </div>
+                    <div class="clipped-border">
+                        <a href="./contenido/historia.php"><img src="./imagenes/historia.jpg" id="clipped"></a>
+                    </div>
 
-            <div class="clipped-border">
-                <a href="./contenido/nosotros.php"><img src="./imagenes/nosotros.png" id="clipped"></a>
-            </div>
+                    <div class="clipped-border">
+                        <a href="./contenido/nosotros.php"><img src="./imagenes/nosotros.png" id="clipped"></a>
+                    </div>
 
-            <div class="clipped-border">
-                <a href="./contenido/alianza.php"><img src="./imagenes/alianza.png" id="clipped"></a>
-            </div>
+                    <div class="clipped-border">
+                        <a href="./contenido/alianza.php"><img src="./imagenes/alianza.png" id="clipped"></a>
+                    </div>
 
-        <div class = "shadow"></div>
-    </div>
+                <div class = "shadow"></div>
+            </div>
  
 
     </div> 
