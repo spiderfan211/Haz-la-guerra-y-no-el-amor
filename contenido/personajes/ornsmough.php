f8f759ec76a908fe07f787ea71066f91fc910370
Emilio <spiderfan211@gmail.com>
2021-02-21 19:58:48 +0100

    Terminados los personajes
    He terminado de redactar las bios de los personajes, añadido unas fotos, editado sus estilos y añadido mi foto de perfil

--- contenido/personajes/ornsmough.php
+++ contenido/personajes/ornsmough.php
@@ -11,7 +11,22 @@
     <body>
         <div id="texto" class="seccion">
 
-                
+            <h1 class="nombrepersonaje">
+                Ornsmough
+            </h1>
+            
+            <p class="parrafopersonaje">
+                <img class="imagenparrafo" src="../../imagenes/paladin.gif" alt="ornsmough">
+                Ornsmough es un paladín consagrado a mantener la virtud 
+                de su pueblo aplastando infieles con su poderoso martillo. 
+                Allá por donde va es un símbolo viviente de la paz y la ley de su 
+                pueblo, y terror para los herejes.
+                <br>
+                En el juego, Ornsmough es un paladín virtuoso. Sus aires de grandeza 
+                y poder no son infundados, pues con un solo golpe de su martillo 
+                podría partir a Ganon en dos. No obstante, su gran debilidad es su ego, 
+                al cual podremos apelar para ganárnoslo.            
+            </p>     
                 
                 
                 
