a1f7ca4e23ce00c50dfc6e839d1bb24b00c5373e
Lysergmoonlight <lysergmoonlighting@gmail.com>
2021-02-24 18:24:26 +0100

    he guardado para volver a la rama master para la exposicion

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
       <li class="hex">
         <div class="hexIn">
           <a class="hexLink" href="#">
-            <div class='img' style="background-image: url(/imagenes/malo maloso.gif)"></div>
+          <img class="img" src="../imagenes/malo maloso.gif">  
+          
             <h1 id="demo1">This is a title</h1>
             <p id="demo2">Some sample text about the article this hexagon leads to</p>
           </a>
@@ -24,8 +25,8 @@
       <li class="hex">
         <div class="hexIn">
           <a class="hexLink" href="#">
-                <div class='img' style='background-image: url(https://images.unsplash.com/44/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.jpg?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;w=1080&amp;fit=max&amp;s=49dab7a5e4b2e28b5707bc2db974c94b);'></div>
-            <h1 id="demo1">This is a title</h1>
+          <img class="img" src="../imagenes/historia.jpg">      
+          <h1 id="demo1">This is a title</h1>
             <p id="demo2">Some sample text about the article this hexagon leads to</p>
           </a>
         </div>
