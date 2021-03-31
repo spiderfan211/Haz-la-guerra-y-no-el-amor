95d6a90469a359f813871d08f1d6b52c81c5f84e
spiderfan211 <spiderfan211@gmail.com>
2021-03-31 17:34:47 +0200

    Ahora sí, bien organizado

---
+++ java/src/com/ceslopedevega/scrum/Loop.java
@@ -0,0 +1,9 @@
+package com.ceslopedevega.scrum;
+
+public class Loop {
+
+	
+	public static void Loop() {
+		
+	}
+}
---
+++ java/src/com/ceslopedevega/scrum/PlayHLGYNEA.java
@@ -0,0 +1,10 @@
+package com.ceslopedevega.scrum;
+
+public class PlayHLGYNEA {
+
+	public static void main(String[] args) {
+		Loop.play();
+
+	}
+
+}
